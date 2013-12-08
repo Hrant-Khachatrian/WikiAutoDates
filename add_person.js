d6fe01f6cdb382181c4a7afcb2c5c9c97878543f
tik-ionut <tmamikonyan@mail.ru>
2013-12-08 14:53:26 +0400

    changing add into  tab ('    ') + add

--- add_person.js
+++ add_person.js
@@ -60,9 +60,9 @@ function month2number(m)
 }
 function addPerson(text, section, NameObject, DayObject, isYear)
 {
-    var add;
-    if(isYear) add="* [[" + DayObject.year + "]] - [[" + NameObject.name + "]]," + NameObject.description+'\n';
-    else add="* [[" + M_names[DayObject.month-1] + 'ի ' + DayObject.day + "]] - [[" + NameObject.name + "]]," + NameObject.description+'\n';
+    var add='   ';
+    if(isYear) add+="* [[" + DayObject.year + "]] - [[" + NameObject.name + "]]," + NameObject.description+'\n';
+    else add+="* [[" + M_names[DayObject.month-1] + 'ի ' + DayObject.day + "]] - [[" + NameObject.name + "]]," + NameObject.description+'\n';
     /// + 'ի ' +   ||   + ' ' +
     var t,sect_name="",section_exists=false;
     for(var i=0;i<text.length-2;i++)
